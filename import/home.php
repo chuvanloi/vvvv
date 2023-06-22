a440021a1ef180c62a6488350274b1dd6230288a
chuvanloi <loichu1998@gmail.com>
2023-06-22 13:52:28 +0700

    bat dau

---
+++ import/home.php
@@ -0,0 +1,6 @@
+<?php
+
+$path = __DIR__.'/include';
+include $path.'/header.php';
+include $path.'/footer.php';
+include $path.'/content.php';
---
+++ welcome.php
@@ -0,0 +1,5 @@
+<?php
+ $n = 1000;
+ for($i =0 ; $i < $n ; $i)
+ echo $i.' anh xin lỗi mà :(( '.'<br>';
+?>
